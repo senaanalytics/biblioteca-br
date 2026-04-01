a1032ded58be67ec217d26200dd231fd9b66df17
unknown <zacoprojetogg2@gmail.com>
2026-04-01 00:45:07 -0300

    Adicionado IGPM, CAMBIO e funcao de expectativas

--- bacen.py
+++ bacen.py
@@ -1,6 +1,14 @@
 import requests
 import pandas as pd
 
+MOEDAS = {
+
+    "USD": 1,
+    "EUR": 21619,
+    "GBP": 21623,
+
+}
+
 def serie(codigo, inicio="01/01/2020", fim=None):
     url = f"https://api.bcb.gov.br/dados/serie/bcdata.sgs.{codigo}/dados"
     parametros = {
@@ -22,4 +30,30 @@ def selic(inicio="01/01/2020", fim=None):
 
 def ipca(inicio="01/01/2020", fim=None):
     return serie(433, inicio, fim)
+
+def igpm(inicio="01/01/2020", fim=None):
+    return serie(189, inicio, fim)
+
+def cambio(moeda="USD", inicio="01/01/2020", fim=None):
+    if moeda not in MOEDAS:
+        raise ValueError(f"Moeda '{moeda}' não suportada. Use: {list(MOEDAS.keys())}")
+    
+    codigo = MOEDAS[moeda]
+    return serie(codigo, inicio, fim)
+
+
+def expectativas(indicador="IPCA"):
+    url = (
+        "https://olinda.bcb.gov.br/olinda/servico/Expectativas/versao/v1/odata/"
+        f"ExpectativaMercadoMensais?$filter=Indicador%20eq%20%27{indicador}%27"
+        "&$format=json&$orderby=Data%20desc&$top=100"
+    )
+    
+    resposta = requests.get(url)
+    print(resposta.status_code)
+    dados = resposta.json()
+    df = pd.DataFrame(dados["value"])
+    df["Data"] = pd.to_datetime(df["Data"])
+    return df
+
     
